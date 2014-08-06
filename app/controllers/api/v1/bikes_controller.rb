6f18750659ee27f240538af20ad2310527cb294b
ilya <ilya.blan4@gmail.com>
2014-08-06 12:40:21 +0700

    rm csrf from api

--- app/controllers/api/v1/bikes_controller.rb
+++ app/controllers/api/v1/bikes_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::BikesController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+  respond_to :json
+
   def register
     push_sender = PushSender.new
     @bike = Bike.find_by(params[:uuid])
@@ -6,6 +9,7 @@ class Api::V1::BikesController < ApplicationController
       render json: {status: :not_found, message: 'Your bike is not registered'}
     else
       bike.update_attribute(:registration_id, params[:registration_id])
+      render json: {status: :ok}
     end
   end
 end
