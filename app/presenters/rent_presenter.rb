271d1f8111503df55121365052bf2316a55c3e3d
ilya <ilya.blan4@gmail.com>
2014-08-06 15:57:17 +0700

    add data to push nootification

--- app/presenters/rent_presenter.rb
+++ app/presenters/rent_presenter.rb
@@ -9,6 +9,6 @@ class RentPresenter
   def wrap
     hash = {openned_at: rent.openned_at, user: rent.user.attributes, code: code, cost: 10}
     hash[:total_cost] = 100 if code == :close
-    hash
+    {data: hash}
   end
 end
