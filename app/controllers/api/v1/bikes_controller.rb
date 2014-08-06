6ab05a7719bd1f12317ff1cb7a3f3e2c3135cdcd
ilya <ilya.blan4@gmail.com>
2014-08-06 12:45:31 +0700

    typo fix

--- app/controllers/api/v1/bikes_controller.rb
+++ app/controllers/api/v1/bikes_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::BikesController < ApplicationController
 
   def register
     push_sender = PushSender.new
-    @bike = Bike.find_by(params[:uuid])
+    @bike = Bike.find_by(uuid: params[:uuid])
     if @bike.nil?
       render json: {status: :not_found, message: 'Your bike is not registered'}
     else
