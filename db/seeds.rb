66ea5cf1dd66735768ec15196bd957a0c8cfcf9d
ilya <ilya.blan4@gmail.com>
2014-08-07 20:47:00 +0700

    add more real stations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Rent.destroy_all
+User.destroy_all
+Station.destroy_all
+Bike.destroy_all
+
 station_1 = Station.create(name: 'Иртышская набережная', latitude: 54.968094, longitude: 73.378906, gates_count: 10)
 station_2 = Station.create(name: 'Омская крепость', latitude: 54.982530, longitude: 73.370534, gates_count: 10)
-
+station_3 = Station.create(name: 'Речной порт', latitude: 54.946384, longitude: 73.375287, gates_count: 10)
+station_4 = Station.create(name: 'Парк культуры и отдыха', latitude: 54.972240, longitude: 73.412647, gates_count: 10)
+station_5 = Station.create(name: 'СКК имени Виктора Блинова', latitude: 54.975311, longitude: 73.394763, gates_count: 10)
+ 
 bike_1 = Bike.create(name: 'default_1', station: station_1, uuid: '8457bf43-81c8-44b1-8ba5-c85575a1a4c8')
 bike_2 = Bike.create(name: 'default_2', station: station_1, uuid: 'f221414e-d4cc-4587-a275-d691e3591c68')
 bike_3 = Bike.create(name: 'default_3', station: station_2, uuid: '0d641816-faa3-4977-8187-202760e7e3e8')
