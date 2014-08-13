cc5ae4086adacf2a980513231a8d2e2d30cdd79e
Ilya Siganov <ilya.blan4@gmail.com>
2014-08-13 17:37:28 +0700

    add logo

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -1,4 +1,12 @@
 class Operator < ActiveRecord::Base
   validates :uid, uniqueness: true, presence: true
   validates :name, presence: true
+
+  def approve
+    update_attribute :approved, true
+  end
+
+  def un_approve
+    update_attribute :approved, false
+  end
 end
