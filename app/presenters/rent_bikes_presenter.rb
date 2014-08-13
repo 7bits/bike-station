d7c423269fe021f07311dd71d471ce1b5891cc6e
Ilya Siganov <ilya.blan4@gmail.com>
2014-08-13 17:05:01 +0700

    add time diff in history

--- app/presenters/rent_bikes_presenter.rb
+++ app/presenters/rent_bikes_presenter.rb
@@ -11,7 +11,7 @@ class RentBikesPresenter
         surname: rent.user.surname,
       },
       rent: {
-        time: (rent.closed_at - rent.openned_at).to_s
+        time: TimeDiff.new.diff(rent.openned_at, rent.closed_at)
       },
       bike: {
         name: rent.bike.name
