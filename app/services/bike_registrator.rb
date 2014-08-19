0ca4d43707cb94804a513c1d209ace7cdc76589f
Ilya Siganov <ilya.blan4@gmail.com>
2014-08-19 18:18:52 +0700

    fix finding by

--- app/services/bike_registrator.rb
+++ app/services/bike_registrator.rb
@@ -3,7 +3,7 @@ class BikeRegistrator
     bike = Bike.find_or_create_by uuid: input.uuid
     raise BikeNotFound.new if bike.nil?
 
-    station = Station.find_by(input.station_id)
+    station = Station.find_by(id: input.station_id)
     raise StationNotFound.new if station.nil?
 
     if bike.update_attributes(registration_id: input.registration_id, name: input.name, station: station)
