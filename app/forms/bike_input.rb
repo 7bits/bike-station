018d62c738ad86b81b0638b17e24724e542a8064
Ilya Siganov <ilya.blan4@gmail.com>
2014-08-19 18:16:26 +0700

    typo fix

--- app/forms/bike_input.rb
+++ app/forms/bike_input.rb
@@ -2,7 +2,7 @@ class BikeInput < Input
   attribute :uuid, String
   attribute :registration_id, String
   attribute :name, String
-  attribute :bike_id, Integer
+  attribute :station_id, Integer
 
-  validates_presence_of :uuid, :registration_id, :name, :bike_id
+  validates_presence_of :uuid, :registration_id, :name, :station_id
 end
