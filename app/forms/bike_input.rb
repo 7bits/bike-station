653cb6eaaba3b02380c2dec11c6e0cb791148d1c
Ilya Siganov <ilya.blan4@gmail.com>
2014-08-19 18:10:10 +0700

    when register bike it connected to station

--- app/forms/bike_input.rb
+++ app/forms/bike_input.rb
@@ -2,6 +2,7 @@ class BikeInput < Input
   attribute :uuid, String
   attribute :registration_id, String
   attribute :name, String
+  attribute :bike_id, Number
 
-  validates_presence_of :uuid, :registration_id, :name
+  validates_presence_of :uuid, :registration_id, :name, :bike_id
 end
--- app/services/bike_registrator.rb
+++ app/services/bike_registrator.rb
@@ -3,11 +3,17 @@ class BikeRegistrator
     bike = Bike.find_or_create_by uuid: input.uuid
     raise BikeNotFound.new if bike.nil?
 
-    # TODO: remove setting station
-    if bike.update_attributes(registration_id: input.registration_id, name: input.name, station: Station.first)
+    station = Station.find_by(input.station_id)
+    raise StationNotFound.new if station.nil?
+
+    if bike.update_attributes(registration_id: input.registration_id, name: input.name, station: station)
       bike
     else
       raise BikeRegistrationError.new(bike.errors.messages)
     end
   end
+
+  class StationNotFound < StandardError; end
+  class BikeNotFound < StandardError; end
+  class BikeRegistrationError < StandardError; end
 end
