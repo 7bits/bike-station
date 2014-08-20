44786bcf49c74c37c8677fc12296c3de62d980ca
ilya <ilya.blan4@gmail.com>
2014-08-20 12:07:32 +0700

    show on map only running bikes

---
+++ db/migrate/20140820050501_show_locations_only_for_bikes_in_use.rb
@@ -0,0 +1,18 @@
+class ShowLocationsOnlyForBikesInUse < ActiveRecord::Migration
+  def up
+    execute %{
+      CREATE OR REPLACE VIEW locations as
+        SELECT bike_id, lat, lng, dates.date, bikes.station_id
+        FROM bike_locations, bikes, (
+          SELECT max(date) as date
+          FROM bike_locations
+          GROUP BY bike_id
+        ) as dates
+        WHERE bikes.in_use = true AND bikes.id = bike_locations.bike_id AND dates.date = bike_locations.date;
+    }
+  end
+
+  def down
+    execute 'DROP VIEW locations'
+  end
+end
