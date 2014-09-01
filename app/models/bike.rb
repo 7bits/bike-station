fa473c7775bf8aed77543388e79411d6c97ddc7f
ilya <ilya.blan4@gmail.com>
2014-09-01 17:37:32 +0700

    fix references to locations in bike model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,7 +2,7 @@ class Bike < ActiveRecord::Base
   has_many :rents
   belongs_to :station
   has_many :bike_locations
-  has_many :locations
+  has_one :locations
 
   scope :unused, -> { where in_use: false }
 end
