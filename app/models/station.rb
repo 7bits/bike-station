661b8db77fa3bb216f355392931dcf405885f428
ilya <ilya.blan4@gmail.com>
2014-09-01 17:37:11 +0700

    rm deprecated unused function

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,8 +4,4 @@ class Station < ActiveRecord::Base
   has_many :incoming_rents, class_name: 'Rent', foreign_key: 'terminal_station_id'
 
   has_many :rents_histories
-
-  def free_gates_count
-    gates_count - bikes.unused.count
-  end
 end
